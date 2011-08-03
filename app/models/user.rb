ce6adc93da1dd363b95da1a7a1c44e8a3fb2d534
Nakarid Rico <nakarid.rico@gmail.com>
2011-08-03 16:05:48 -0430

    Finished user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@
 require 'digest'
 class User < ActiveRecord::Base
   attr_accessor   :password, :password_confirmation, :salt
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation, :salt
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
